e21920a0dcad330ea92cd7fc67b7a2db9444bc3a
Blaine Lafreniere <brlafreniere@gmail.com>
2024-05-17 21:46:29 -0600

    fix app.yaml, add ingredients

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render inline: "<h1>Hello</h1><p>from Microservice A.</p>"
+  end
 end
